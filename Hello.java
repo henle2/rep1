c9a566ceaea0801392146737125a7cc104ddf6a7
henle2 <henle2@mta.ac.il>
2024-05-11 19:33:34 +0300

    changeeeee

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello{
 public static void main (String[] args) {
 for (int i=1;i<=10;i++){
-System.out.println("Hello World chen  !!!!!"+i);
+System.out.println("Hellon World chen  !!!!!"+i);
 }
 }
 }
